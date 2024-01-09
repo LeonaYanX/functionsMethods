d07d6e3453caf3f14d41f0fecfb88c5815f6f152
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-09 23:28:34 +0400

    Task 1 done

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Написать метод который, выводит на экран строку , синволы из которых состоит строка и их количество 
+// вводится пользователем.
+// static void PrintRaw(string simvol , int count)
+// {
+//    for(int i = 0 ; i<count ; i++)
+//     {
+//         Console.Write(simvol + " ");
+//     }
+// }
+//    Console.WriteLine("Enter the simvol");
+//    string simvol = Console.ReadLine();
+//    Console.WriteLine("Enter how many times I must write it in a raw");
+//    int count = Convert.ToInt32(Console.ReadLine());
+//  PrintRaw(simvol ,count);
+
+// Написать метод для поиска индекса элемента массива типа инт , метод должен вернуть индекс первого най-
+// денного элемента , и обработать случай если его нет.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static int MyFindIndex(int value, int[] Array)
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                switch(Array[i])
+                {
+                    case value:
+                    return i;
+                    break;
+                    default:
+                    System.Console.WriteLine("There is no such a elemnt");
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("Enter the size of the Array");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = new int[size];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine($"Enter the element {i + 1}");
+            myArray[i] = Convert.ToInt32(Console.WriteLine());
+        }
+        System.Console.WriteLine("Enter the element you want to find");
+        int value = int.Parse(Console.ReadLine());
+        WriteLine(MyFindIndex(value, myArray));
+    }
+}
